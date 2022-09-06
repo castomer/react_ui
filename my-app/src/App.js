4463582fcb78cafa1c4986b05209e218d3be5c50
Роман <roman@MacBook-Air-Roman.local>
2022-09-06 21:24:27 +0300

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
+import CostItems  from "./components/CostItems";
 
 function App() {
   return (
     <div>
-      <h1>
-          hello
-      </h1>
+      <h1>hello</h1>
+      <CostItems></CostItems>
     </div>
   );
 }
---
+++ my-app/src/components/CostItems.js
@@ -0,0 +1,18 @@
+import "./CostItems.css";
+
+function CostItems() {
+    return (
+        <div className='cost-item'>
+            <div>Март 2021 </div>
+            <div className='cost-item_descryption'>Март 2021 </div>
+                <div>
+                    <h2>Холодильник</h2>
+                    <div className=' cost-item__price'>999$</div>  
+                </div>
+        </div>
+    );
+
+    
+}
+ 
+export default CostItems
